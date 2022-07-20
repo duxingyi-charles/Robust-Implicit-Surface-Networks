d2ef173ce833b9aa6d49cc58020394569b6aab4a
CharlesDu <dohoney@126.com>
2022-07-20 17:43:30 -0500

    one header to include many headers

---
+++ src/robust_implicit_networks.h
@@ -0,0 +1,16 @@
+//
+// Created by Charles Du on 7/20/22.
+//
+
+#ifndef ROBUST_IMPLICIT_NETWORKS_ROBUST_IMPLICIT_NETWORKS_H
+#define ROBUST_IMPLICIT_NETWORKS_ROBUST_IMPLICIT_NETWORKS_H
+
+#include "io.h"
+#include "mesh.h"
+#include "extract_mesh.h"
+#include "mesh_connectivity.h"
+#include "pair_faces.h"
+#include "topo_ray_shooting.h"
+#include "cell_connectivity.h"
+
+#endif //ROBUST_IMPLICIT_NETWORKS_ROBUST_IMPLICIT_NETWORKS_H
