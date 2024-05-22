f9a717e375f4d2bea6e9b455ae5c6fa4b5f96409
yiwen.ju <yiwen.ju@wustl.edu>
2024-05-22 12:51:34 -0400

    header file illustrations (All functions I'm certain about)

--- src/mesh_connectivity.h
+++ src/mesh_connectivity.h
@@ -36,7 +36,7 @@ void compute_patches(const std::vector<std::vector<size_t>> & edges_of_face,
 ///
 ///@param[in] iso_verts             the list of vertices on the mesh. Each vertex contains 1-3 implicit function labesl depending on its connectivity. This purely serves as a verification for the face labels above.
 ///
-///@param[out] Bool            True - passed the patch label checks / False - failed the check
+///@return Bool            True - passed the patch label checks / False - failed the check
 bool check_patch_label(const std::vector<std::vector<size_t>> & edges_of_face,
                      const std::vector<Edge>& mesh_edges,
                      const std::vector<PolygonFace>& mesh_faces,
@@ -54,6 +54,7 @@ void compute_patches(const std::vector<std::vector<size_t>> & edges_of_face,
 ///
 /// @param[in] mesh_edges           As above
 /// @param[out] non_manifold_edges_of_vert          Indices of non-manifold vertices
+///
 /// @param[out] chains          Chains of non-manifold edges; encoded by a vector of edge indices.
 void compute_chains(const std::vector<Edge>& mesh_edges,
                     const std::vector<std::vector<size_t>>& non_manifold_edges_of_vert,
@@ -63,6 +64,12 @@ void compute_chains(const std::vector<Edge>& mesh_edges,
 /// each shell is a list of half-patches
 /// each component is a list of patches
 /// we also build maps: half-patch --> shell,  patch --> component
+///@param[in] num_patch         Patch number
+///@param[in] half_patch_pair_list          The list of half patch represented as (patch i, 1) and (patch i, -1); Maps to a single half-patch index: (patch i, 1) <--> 2i,  (patch i, -1) <--> 2i+1
+///
+/// @param[out] shell_of_patch          Map: half-patch --> shell
+/// @param[out] component           Connected componeent represented as a list of patches
+/// @param[out] component_of_patch          Map: patch --> component
 void compute_shells_and_components(size_t num_patch,
                                    const std::vector<std::vector<std::pair<std::pair<size_t, int>,std::pair<size_t, int>>>>& half_patch_pair_list,
                                    std::vector<std::vector<size_t>>& shells,
@@ -72,6 +79,11 @@ void compute_shells_and_components(size_t num_patch,
 );
 
 /// group shells into arrangement cells
+///
+///@param[in] num_shell         Shells number
+///@param[in] shell_links           The connectivity of shells
+///
+///@param[out] arrangement_cells            Cells
 void compute_arrangement_cells(size_t num_shell,
                                const std::vector<std::pair<size_t,size_t>> &shell_links,
                                std::vector<std::vector<size_t>>& arrangement_cells);
